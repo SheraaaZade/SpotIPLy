75ca651c8aeefcef62dc1f38b17778db4fa6017f
Chehrazad Ouazzani <113687531+SheraaaZade@users.noreply.github.com>
2024-01-06 18:33:28 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+  public static void main(String[] args) {
+    Album lonerism = new Album.Builder("Lonerism",
+        "Tame Impala")
+        .setCountry("Australie")
+        .setYearReveal(2012)
+        .setGenre("indie rock")
+        .build();
+
+    Album orange = new Album.Builder("channel ORANGE",
+        "Frank Ocean")
+        .setYearReveal(2012)
+        .setGenre("R&B")
+        .build();
+
+    Album visions = new Album.Builder("Visions",
+        "Grimes")
+        .setYearOriginalReveal(2012)
+        .setLabel("4AD")
+        .setGenre("Electronic")
+        .build();
+    System.out.println("-----ALBUM-----");
+    System.out.println(lonerism);
+    System.out.println(orange);
+    System.out.println(visions);
+  }
 }
